f92418dea1fa5f6225bf7de476376a5ebd468a36
docgen <docgen>
2024-10-09 21:19:56 +0800

    config image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+    output: "standalone",
+    reactStrictMode: false,
+    images: {
+        domains: ["nft1000.oss-cn-beijing.aliyuncs.com"]
+    }
+};
 
 export default nextConfig;
